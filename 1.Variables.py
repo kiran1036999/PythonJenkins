5e48b0db5910aeb075f3a2cdba87fd0fa218877c
kiran1036999 <103207638+kiran1036999@users.noreply.github.com>
2023-02-01 13:14:27 +0530

    var

---
+++ 1.Variables.py
@@ -0,0 +1,17 @@
+var1 = "kiran"
+print(var1)
+
+a,b,c = 5, 2.3, "8"
+print(a)
+print("\n")
+print(b)
+print("\n")
+print(c)
+print("\n")
+
+var1 = var2 = var3 = "Same Value"
+print(var1)
+print("\n")
+print(var2)
+print("\n")
+print(var3)
